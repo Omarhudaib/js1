d058c3749df260b4cea36a6b9a4328672ac970d9
Omarhudaib <168991559+Omarhudaib@users.noreply.github.com>
2024-06-03 22:56:59 +0300

    Add files via upload

---
+++ loops_basic.js
@@ -0,0 +1,76 @@
+/*
+
+// 1- Write a program that prints numbers from 1 to 10 using a for loop.
+let f;
+for(f =1 ; f <= 10; f++ ){
+    console.log(f);
+}
+// 2-Write a program that prints the even numbers from 1 to 10 using a for loop.
+
+let u;
+for(u =1 ; u <= 10; u++ ){
+    if(u % 2 == 0){
+        console.log(u)
+    }
+}
+
+// 3- Write a program that prints the odd numbers from 1 to 10 using a while loop.
+
+let p;
+for(p =1 ; p <= 10; p++ ){
+    if(p % 2 !== 0){
+        console.log(p)
+    }
+}
+// 4- Write a program that prints the multiplication table of a number entered by the user using a for loop.
+let o;
+// 6- Write a program that calculates the factorial of a number entered by the user using a for loop.
+for(o =0 ; o <= 10; o++ ){
+        console.log(o*x)
+    }
+
+
+// 5- Write a program that calculates the sum of numbers from 1 to 100 using a while loop.
+let sum =0;
+let r =0;
+while(r < 101 ){
+sum= sum+r
+        console.log(sum)
+        r++;
+    }
+
+
+// 6- Write a program that calculates the factorial of a number entered by the user using a for loop.
+
+let x = parseInt(prompt("enter valuo"));
+
+for( let v =1 ;  v<= x; v++ ){
+console.log(x*(x-1));
+    }
+      */
+    // 7- Write a program that prints the Fibonacci series up to a certain number entered by the user using a while loop.
+/*
+let u= parseInt(prompt("enternum"));
+let i =1;
+let s =0;
+while(i == u ){
+console.log(s);
+console.log("+")
+console.log(i);
+i=s;
+s=i+s;
+
+}
+*/
+
+// 8- Write a program that prints the reverse of the following numbers:
+//5 , 10 , 15 , 20
+// using a while loop.
+/*
+
+let i =20;
+while( i > 0 ){
+    console.log(i)
+    i=i-5 
+}
+*/
